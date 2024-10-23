3349a49ca7ad42cf263b04314243946ce0e7674d
Marvin A. Ruder <signed@mruder.dev>
2024-10-23 15:14:36 +0200

    Make log levels configurable
    Signed-off-by: Marvin A. Ruder <signed@mruder.dev>

--- internal/logger/main.go
+++ internal/logger/main.go
@@ -3,9 +3,27 @@ package logger
 import (
 	"log/slog"
 	"os"
+	"strings"
 )
 
-var logger = *slog.New(slog.NewTextHandler(os.Stdout, nil))
+func getLogLevel(level string) slog.Level {
+	switch strings.ToUpper(level) {
+	case "DEBUG":
+		return slog.LevelDebug
+	case "INFO":
+		return slog.LevelInfo
+	case "WARN":
+		return slog.LevelWarn
+	case "ERROR":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
+}
+
+var logger = *slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+	Level: getLogLevel(os.Getenv("LOG_LEVEL")),
+}))
 
 var (
 	Error = logger.Error
